497ca208bb16936c98b359b6211e07880c5159c2
Molly Huang <molly.h@gmail.com>
2021-04-25 17:57:06 +0800

    #2-2. Writing a Unit Test with Just - e.g. logout button 1. Create a test suite (a block of tests) 2. Set up your test(s) 3. Mount the component with vue-test-utils 4. Set data, if necessary 5. Assert what the result should be

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,30 @@
 import AppHeader from "@/components/AppHeader"
+import { mount } from '@vue/test-utils'
 
+// 1. Create a test suite (a block of tests)
 describe("AppHeader", () => {
+  // Put test(s) here
+
+  // 2. Set up your test(s)
   test("If user is not logged in, do not show logout button", () => {
-    expect(true).toBe(true);
+    // testing function
+
+    // 3. Mount the component with vue-test-utils
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('button').isVisible()).toBe(false)
   });
 
-  test("If user is logged in, show logout button", () => {
-    expect(true).toBe(true);
+  // Using async/await when waiting on DOM updates
+  test("If user is logged in, show logout button", async () => {
+    // testing function
+    const wrapper = mount(AppHeader)
+
+    // 4. Set data, if necessary
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick()
+
+    // 5. Assert what the result should be
+    expect(wrapper.find('button').isVisible()).toBe(true)
   });
 });
